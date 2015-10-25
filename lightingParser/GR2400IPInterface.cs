d266b92c53b2e5e4f326482b13b42d6408dc00b5
Piense <piense@gmail.com>
2015-10-25 10:54:27 -0700

    Lighting Data Log

--- lightingParser/GR2400IPInterface.cs
+++ lightingParser/GR2400IPInterface.cs
@@ -70,12 +70,12 @@ namespace lightingParser
                 logEntry.Title = "Heartbeat";
                 logEntry.Description = "Heartbeat from GR2400 system.";
             }
-                
+
             LightingDataLog.Add(logEntry);
             u.BeginReceive(new AsyncCallback(OnNewMessage), ((UdpState)(ar.AsyncState)));
 
             EventHandler handler = NewMessage;
-            if (handler != null) { handler(this, new MessageEventArgs(receiveString,receiveBytes)); }
+            if (handler != null) { handler(this, new MessageEventArgs(receiveString, receiveBytes)); }
         }
 
         public void QueryLCDData()
@@ -92,24 +92,24 @@ namespace lightingParser
 
         private void sendData(string sendbuf, string title, string description)
         {
-            sendData(Encoding.ASCII.GetBytes(sendbuf),title,description);
+            sendData(Encoding.ASCII.GetBytes(sendbuf), title, description);
         }
 
         public void ScrollUp()
         {
-            sendData("38d0" + (char)0x0d + "38d108" + (char)0x0d + "380600fe3c" + (char)0x0d + (char)0x0a,"Scroll Up Key Down","Scroll Up Key Down");
-            sendData("38d0" + (char)0x0d + "38d108" + (char)0x0d + "380600ff3d" + (char)0x0d + (char)0x0a,"Scroll Up Key Up","Scroll Up Key Up");
+            sendData("38d0" + (char)0x0d + "38d108" + (char)0x0d + "380600fe3c" + (char)0x0d + (char)0x0a, "Scroll Up Key Down", "Scroll Up Key Down");
+            sendData("38d0" + (char)0x0d + "38d108" + (char)0x0d + "380600ff3d" + (char)0x0d + (char)0x0a, "Scroll Up Key Up", "Scroll Up Key Up");
         }
 
         public void ScrollDown()
         {
-            sendData("38d0" + (char)0x0d + "38d108" + (char)0x0d + "380600fd3b" + (char)0x0d + (char)0x0a, "Scroll Down Key Down","Scroll Down Key Down");
-            sendData("38d0" + (char)0x0d + "38d108" + (char)0x0d + "380600ff3d" + (char)0x0d + (char)0x0a, "Scroll Down Key Up","Scroll Down Key Up");
+            sendData("38d0" + (char)0x0d + "38d108" + (char)0x0d + "380600fd3b" + (char)0x0d + (char)0x0a, "Scroll Down Key Down", "Scroll Down Key Down");
+            sendData("38d0" + (char)0x0d + "38d108" + (char)0x0d + "380600ff3d" + (char)0x0d + (char)0x0a, "Scroll Down Key Up", "Scroll Down Key Up");
         }
 
         public void Exit()
         {
-            sendData("38d0" + (char)0x0d + "38d108" + (char)0x0d + "380600df1d" + (char)0x0d + (char)0x0a, "Exit Key Down","Exit Key Down");
+            sendData("38d0" + (char)0x0d + "38d108" + (char)0x0d + "380600df1d" + (char)0x0d + (char)0x0a, "Exit Key Down", "Exit Key Down");
             sendData("38d0" + (char)0x0d + "38d108" + (char)0x0d + "380600ff3d" + (char)0x0d + (char)0x0a, "Exit Key Down", "Exit Key Up");
         }
 
@@ -121,26 +121,50 @@ namespace lightingParser
 
         public void Help()
         {
-            sendData("38d0" + (char)0x0d + "38d108" + (char)0x0d + "3806007fbd" + (char)0x0d + (char)0x0a, "Help Key Down","Help Key Down");
-            sendData("38d0" + (char)0x0d + "38d108" + (char)0x0d + "380600ff3d" + (char)0x0d + (char)0x0a, "Help Key Up","Help Key Up");
+            sendData("38d0" + (char)0x0d + "38d108" + (char)0x0d + "3806007fbd" + (char)0x0d + (char)0x0a, "Help Key Down", "Help Key Down");
+            sendData("38d0" + (char)0x0d + "38d108" + (char)0x0d + "380600ff3d" + (char)0x0d + (char)0x0a, "Help Key Up", "Help Key Up");
         }
 
         public void Enter()
         {
-            sendData("38d0" + (char)0x0d + "38d108" + (char)0x0d + "380600ef2d" + (char)0x0d + (char)0x0a,"Enter Key Down","Help Key Down");
-            sendData("38d0" + (char)0x0d + "38d108" + (char)0x0d + "380600ff3d" + (char)0x0d + (char)0x0a,"Enter Key Up","Enter Key Up");
+            sendData("38d0" + (char)0x0d + "38d108" + (char)0x0d + "380600ef2d" + (char)0x0d + (char)0x0a, "Enter Key Down", "Help Key Down");
+            sendData("38d0" + (char)0x0d + "38d108" + (char)0x0d + "380600ff3d" + (char)0x0d + (char)0x0a, "Enter Key Up", "Enter Key Up");
         }
 
         public void TabUp()
         {
-            sendData("38d0" + (char)0x0d + "38d108" + (char)0x0d + "380600fb39" + (char)0x0d + (char)0x0a,"Tab Up Key Down","Tab Up Key Down");
-            sendData("38d0" + (char)0x0d + "38d108" + (char)0x0d + "380600ff3d" + (char)0x0d + (char)0x0a,"Tab Up Key Up","Tab Up Key Up");
+            sendData("38d0" + (char)0x0d + "38d108" + (char)0x0d + "380600fb39" + (char)0x0d + (char)0x0a, "Tab Up Key Down", "Tab Up Key Down");
+            sendData("38d0" + (char)0x0d + "38d108" + (char)0x0d + "380600ff3d" + (char)0x0d + (char)0x0a, "Tab Up Key Up", "Tab Up Key Up");
         }
 
         public void TabDown()
         {
-            sendData("38d0" + (char)0x0d + "38d108" + (char)0x0d + "380600f735" + (char)0x0d + (char)0x0a,"Tab Down Key Down","Tab Down Key Down");
-            sendData("38d0" + (char)0x0d + "38d108" + (char)0x0d + "380600ff3d" + (char)0x0d + (char)0x0a,"Tab Down Key Up","Tab Down Key Up");
+            sendData("38d0" + (char)0x0d + "38d108" + (char)0x0d + "380600f735" + (char)0x0d + (char)0x0a, "Tab Down Key Down", "Tab Down Key Down");
+            sendData("38d0" + (char)0x0d + "38d108" + (char)0x0d + "380600ff3d" + (char)0x0d + (char)0x0a, "Tab Down Key Up", "Tab Down Key Up");
+        }
+
+        public string QueryID(int id)
+        {
+            string ToSend = "3820" + (char)0x0d + "37" + id.ToString("x2") + ((int)((55 + id) % 255)).ToString("x2") + (char)0x0d + (char)0x0a;
+            byte[] sendbuf = Encoding.ASCII.GetBytes(ToSend);
+
+            if (udpClient != null && serverEndpoint != null)
+                udpClient.Send(sendbuf, sendbuf.Length, serverEndpoint);
+
+
+            //byte[] retBuf = udpClient.Receive(ref receiveEndpoint);
+            //MessageBox.Show(System.Text.Encoding.ASCII.GetString(retBuf));
+
+            /*
+                1   3F0E30631011B
+                30  3F0E302320219
+                34  3F0E304350521
+
+                6 button switch: 3F0E1000600E7
+                nothing: 3F00000000000
+                Heartbaet: 3B06F801C1FB
+             */
+            return "";
         }
 
         private void beginListening()
